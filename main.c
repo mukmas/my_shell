d7cc29bcea27b8c5c118899dfd399d80f152e42e
Lukas Hansen <mukmas00@gmail.com>
2025-10-27 17:08:56 +0100

    Fix memory leak when realloc fails

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 
 int main()
 {
-    char *string = NULL;
+    char *line = NULL;
     size_t size = 0;
     ssize_t chars_read;
 
-    chars_read = getline(&string, &size, stdin);
+    chars_read = getline(&line, &size, stdin);
 
     int count = 0;
     char **tokens = NULL;
-    char *token = strtok(string, " ");
+    char *token = strtok(line, " ");
 
     char **temp = NULL;
     while (token != NULL)
@@ -20,16 +20,17 @@ int main()
         temp = realloc(tokens, sizeof(char *) * (count + 1));
         if (temp == NULL)
         {
+            free(tokens);
+            free(line);
             perror("Failed to allocate memory");
             exit(EXIT_FAILURE);
         }
         temp[count++] = token;
         token = strtok(NULL, " ");
     }
-    tokens = temp;
 
     free(tokens);
-    free(string);
+    free(line);
 
     return 0;
 }
