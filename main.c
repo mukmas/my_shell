b066943b478c3b9d9bdc4fe655a51d592309636a
Lukas Hansen <mukmas00@gmail.com>
2025-10-28 22:36:39 +0100

    Separate input and tokenization into functions

--- main.c
+++ main.c
@@ -2,7 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+char *get_input(void);
+void tokenize_input(int *count, char ***tokens, char *input);
+
 int main()
+{
+
+    char *input = get_input();
+    if (input == NULL)
+    {
+        perror("Failed to read input");
+        return 1;
+    }
+
+    int count = 0;
+    char **tokens = NULL;
+    tokenize_input(&count, &tokens, input);
+
+    for (int i = 0; i < count; i++)
+    {
+        free(tokens[i]);
+    }
+    free(tokens);
+
+    return 0;
+}
+
+char *get_input(void)
 {
     char *line = NULL;
     size_t size = 0;
@@ -10,32 +36,33 @@ int main()
 
     chars_read = getline(&line, &size, stdin);
 
-    int count = 0;
-    char **tokens = NULL;
-    char *token = strtok(line, " \n");
+    if (chars_read == -1)
+    {
+        free(line);
+        return NULL;
+    }
+
+    return line;
+}
+
+void tokenize_input(int *count, char ***tokens, char *input)
+{
+    char *token = strtok(input, " \n");
 
     char **temp = NULL;
     while (token != NULL)
     {
-        temp = realloc(tokens, sizeof(char *) * (count + 1));
+        temp = realloc(*tokens, sizeof(char *) * (*count + 1));
         if (temp == NULL)
         {
-            free(tokens);
-            free(line);
+            free(*tokens);
+            free(input);
             perror("Failed to allocate memory");
             exit(EXIT_FAILURE);
         }
-        tokens = temp;
-        temp[count++] = strdup(token);
+        *tokens = temp;
+        temp[*count++] = strdup(token);
         token = strtok(NULL, " \n");
     }
-
-    for (int i = 0; i < count; i++)
-    {
-        free(tokens[i]);
-    }
-    free(tokens);
-    free(line);
-
-    return 0;
+    free(input);
 }
